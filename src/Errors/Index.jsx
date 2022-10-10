aaa23b03c8d55487ff96f6884b0be1a343201dbc
Kafriansyah Kafriasnyah <kafri@prevalensi.com>
2022-10-10 16:37:19 +0700

    fontt mono

--- src/Errors/Index.jsx
+++ src/Errors/Index.jsx
@@ -4,7 +4,7 @@ export default function Error() {
     return (
         <>
             <div className='grid place-content-center min-h-screen bg-slate-700'>
-                <h1 className='text-red-600 font-semibold text-xl'>Error</h1>
+                <h1 className='text-red-600 font-semibold font-mono text-xl'>Error</h1>
             </div>
         </>
     )
