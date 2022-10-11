4ca911db8afaba5e3ee712fc031ebd477b69e492
Kafriansyah Kafriasnyah <kafri@prevalensi.com>
2022-10-11 08:26:08 +0700

    manage component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function Card({ children }) {
+    return <div className='w-full bg-white  tracking-normal shadow-md rounded'>{children}</div>
+}
+
+function Title({ children }) {
+    return (
+        <div className='border-b-4 border-slate-200 p-4'>
+            <div className='text-lg'> {children}</div>
+        </div>
+    )
+}
+
+function Body({ children }) {
+    return (
+        <div className='bg-slate-50 p-4 leading-relaxed'>
+            <div className='text-base'>{children}</div>
+        </div>
+    )
+}
+
+function Footer({ children }) {
+    return (
+        <div className='flex justify-end bg-slate-100 p-4'>
+            <div className='text-sm'>{children}</div>
+        </div>
+    )
+}
+
+Card.Title = Title
+Card.Body = Body
+Card.Footer = Footer
+
+export default Card
